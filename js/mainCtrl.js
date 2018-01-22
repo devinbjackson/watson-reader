8b4499f034bc08be662e7df5e67c667ab10bfd4a
Devin B Jackson <mrdevidu@gmail.com>
2018-01-21 20:32:08 -0600

    chore: clean up

--- js/mainCtrl.js
+++ js/mainCtrl.js
@@ -1,11 +1,17 @@
 angular.module('app').controller('mainCtrl', function($scope, mainSrvc) {
     $scope.username = 'World';
     $scope.quote = {};
-    $scope.voice = 'en-US_AllisonVoice';
-    mainSrvc.getQuotes().then(
+    $scope.voice = 'en-US_LisaVoice';
+    $scope.newQuote = function(){
+        mainSrvc.getQuote().then(
         response => (
           ($scope.quote = response.data), console.log(response.data)
         )
-    );
-    $scope.speak = mainSrvc.getVoice
+    )
+};
+    $scope.generateAudio = mainSrvc.getVoice;
+    $scope.speak = function() {
+     var audio = new Audio('../audio/quote_audio.wav');
+     audio.play();
+};
   });
--- js/mainSrvc.js
+++ js/mainSrvc.js
@@ -1,9 +1,10 @@
 angular.module('app').service('mainSrvc', function($http) {
-    this.getQuotes = function(){
+    this.getQuote = function(){
          return $http.get("http://localhost:3003/api/quotes")
         .then(response => response)
       };
     this.getVoice = function(text, voice){
+        console.log(text)
         return $http.get(`http://localhost:3003/api/voice/${text}/${voice}`)
        .then(response => response)
      };  
